c8d858cbfa12e22f2abfbe3afd91894787f9bbb6
venkatesh j m <125080129+venkisamarth@users.noreply.github.com>
2024-07-17 20:06:07 +0530

    Add files via upload

---
+++ python_9.py
@@ -0,0 +1,18 @@
+#How python works? 
+# python is complailed or interprited 
+# internal excecution of python porgram 
+#many more question 
+
+# source code -> dompiler or inertpreter  -> Binary -> machine 
+# way of working diffrent for compailer and interpreter 
+# c/c++ -> compliler -> .exc file -> computer 
+# python -compiler -> interpreter -> compyter 
+# write code -> file.py -> compiler -> bytecode opetional.pyc->having extention.py -> pvm python vertual machine 
+
+# computer the executable file in the line that could be the string
+phase 1 (Development)
+
+calculator app-> file.py-> compiler-> file.pyc
+phase 2 (acutal implementation )
+
+file.pyc -> pvm (interpreter )
